0ba63513919943c64b7832f367c2f44a3505b67e
Ryan Antkowiak <antkowiak@gmail.com>
2013-10-15 13:27:32 -0500

    function protos

--- src/global.h
+++ src/global.h
@@ -6,4 +6,14 @@ void print_error(const char *msg);
 
 TNumber calculate_mort(TNumber principal, TNumber interest_rate, TNumber num_years);
 TNumber calculate_pmt(TNumber r, TNumber nper, TNumber pv, TNumber fv, TNumber type);
+TNumber calculate_rand(TNumber a);
+TNumber calculate_srand(TNumber a);
+TNumber calculate_time();
+
+TNumber calculate_eq(TNumber a, TNumber b);
+TNumber calculate_ne(TNumber a, TNumber b);
+TNumber calculate_lt(TNumber a, TNumber b);
+TNumber calculate_le(TNumber a, TNumber b);
+TNumber calculate_gt(TNumber a, TNumber b);
+TNumber calculate_ge(TNumber a, TNumber b);
 
